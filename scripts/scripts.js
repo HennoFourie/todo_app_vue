b4e8ffab370503f1874f45ad67c42bf7281d53aa
HennoFourie <fourie.henno@gmail.com>
2021-07-14 23:52:34 +0200

    Due date added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,15 @@
-const shoppingList = new Vue({
-    el: '#vue',
-    data: {
-      newItem: '',
-      items: [
-        {
-          id: 1,
-          name: 'Clean the fridge',
-          completed: false,
-        },
-        {
-          id: 2,
-          name: 'Walk the dogs',
-          completed: true,
-        },
-      ],
-    },
-    computed: {
-      reversedItems() {
-        return this.items.slice(0).reverse();
-      },
-    },
-    methods: {
-      addItem: function () {
-        this.items.push({
-          id: this.items.length + 1,
-          name: this.newItem,
-          completed: false,
-        });
-        this.newItem = '';
-      },
-      toggleCompleted: function (item) {
-        item.completed = !item.completed;
-      },
-      removeItem: function (item) {
-        this.items = this.items.filter((newItem) => newItem.name !== item.name);
-      },
-    },
-  });
+var STORAGE_KEY = "todos-vuejs-2.0";
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
   
